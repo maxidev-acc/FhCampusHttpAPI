5849b0550350e68dd55d94b7a374ac6b3db07036
maximilian_r <126189043+maxidev-acc@users.noreply.github.com>
2024-05-28 17:02:41 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/maxim/Documents/GitHub/SoftwareEntwicklungC
-# Build directory: C:/Users/maxim/Documents/GitHub/SoftwareEntwicklungC/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
